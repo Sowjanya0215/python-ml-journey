8217ed1533bf4c4dc364cd68cbb062cc5e8daf31
Sowjanya0215 <dasarisowjanya04@gmail.com>
2026-02-17 19:53:47 +0530

    Createted basic.py

---
+++ Week-01/Day-01/Week-01/Day-01/basic.py
@@ -0,0 +1,28 @@
+# Even or Odd
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Largest of 3 Numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print("Largest:", a)
+elif b >= a and b >= c:
+    print("Largest:", b)
+else:
+    print("Largest:", c)
+
+
+# Print numbers from 1 to 5
+
+for i in range(1, 6):
+    print(i)

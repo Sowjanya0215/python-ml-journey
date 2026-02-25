3e911bf6110525095ede19744c7693c364f51ef8
Sowjanya0215 <dasarisowjanya04@gmail.com>
2026-02-25 23:18:08 +0530

    practiced Arthematic operatons

---
+++ Week-01/Day-05/Arthematic_operators.py
@@ -0,0 +1,24 @@
+#ARTHEMATIC OPERATIONS---
+import numpy as np
+a=np.array([[1,2,3],[6,7,8]])
+b=np.array([[5,6,5],[8,7,6]])
+
+#addition
+c=np.add(a,b)
+print(c)
+
+#Subtraction
+d=np.subtract(a,b)
+print(d)
+
+#Division
+e=np.divide(a,b)
+print(e)
+
+#modulus
+f=np.mod(a,b)
+print(f)
+
+#power
+g=np.power(a,b)
+print(g)
